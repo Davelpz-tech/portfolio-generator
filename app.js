0148041a812b7c2ab09baac53a664f1a2a30e5dc
Davelpz-tech <davelpz15@gmail.com>
2021-06-21 17:14:56 -0700

    adding inquirer and setting up a question prompt

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
-const fs = require('fs');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const generatePage = require('./src/page-template.js');
+// const pageHTML = generatePage(name, github);
 
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw err;
 
+//     console.log("Portfolio complete! Check out the index.html to see the output!")
+// });
 
+const inquirer = require('inquirer');
 
-
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
 
 
 
